77da7b6f77936f10a9f44ef941bb1744a8b6613c
thomas kichelm <thomas.kichelm01@gmail.com>
2023-12-28 18:00:12 +0100

    Rediraection vers app GPS

--- src/components/WaterPointsMap.js
+++ src/components/WaterPointsMap.js
@@ -1,29 +1,30 @@
-// src/components/WaterPointsMap.js
+// WaterPointsMap.js
 
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'; // Assurez-vous d'importer les styles CSS de Leaflet
+import 'leaflet/dist/leaflet.css';
 import './styles.css';
 
 import L from 'leaflet';
-import iconUrl from '../icon.png'; // Remplacez ceci par le chemin vers votre image PNG
+import iconUrl from '../icon.png';
+
 const customIcon = L.icon({
-    iconUrl: iconUrl,
-    iconSize: [32, 32], // Taille de l'icône
-    iconAnchor: [16, 32], // Point d'ancrage de l'icône
-    popupAnchor: [0, -32], // Position de la popup par rapport à l'icône
-  });
+  iconUrl: iconUrl,
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
 
 const WaterPointsMap = () => {
   const [waterPoints, setWaterPoints] = useState([]);
-  const apiUrl = 'https://owaterorg.gogocarto.fr/api/elements.json?categories=&bounds=-0.74707%2C43.77109%2C0.59326%2C44.71551';
+  const apiUrl = 'https://owater.gogocarto.fr/api/elements?categories=&bounds=-0.74707%2C43.77109%2C0.59326%2C44.71551';
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(apiUrl);
-        setWaterPoints(response.data.data || []); // Assurez-vous que response.data.data est défini ou initialisez-le à un tableau vide
+        setWaterPoints(response.data.data || []);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -33,9 +34,9 @@ const WaterPointsMap = () => {
   }, []);
 
   return (
-    <div map-container>
+    <div className="map-container">
       <h1>Carte des points d'eau potable</h1>
-      <MapContainer center={[43.946, 0.719]} zoom={10} style={{ height: '500px' }}>
+      <MapContainer center={[43.946, 0.719]} zoom={10} className="custom-map">
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">
@@ -44,12 +45,22 @@ const WaterPointsMap = () => {
         {waterPoints && waterPoints.map(point => (
           <Marker
             position={[point.geo.latitude, point.geo.longitude]}
-            icon={customIcon} // Utilisez l'icône WaterIcon comme marqueur
+            icon={customIcon}
             key={point.id}
           >
             <Popup>
               <div>
-                <h2>{point.name}</h2> 
+                <h2>{point.name}</h2>
+                {/* Lien pour ouvrir l'application GPS avec les coordonnées pour l'itinéraire */}
+                <p>
+                  <a
+                    href={`geo:${point.geo.latitude},${point.geo.longitude}?q=${point.geo.latitude},${point.geo.longitude}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Ouvrir l'application GPS pour obtenir l'itinéraire
+                  </a>
+                </p>
               </div>
             </Popup>
           </Marker>
@@ -57,6 +68,6 @@ const WaterPointsMap = () => {
       </MapContainer>
     </div>
   );
-}; 
+};
 
 export default WaterPointsMap;
