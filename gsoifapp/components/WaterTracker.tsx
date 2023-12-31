17fe33d13fdec3fef9299509a0676847b32336cc
thomas kichelm <thomas.kichelm01@gmail.com>
2023-12-31 18:04:52 +0100

    Ajout graph quotidien tracker Eau

--- gsoifapp/components/WaterTracker.tsx
+++ gsoifapp/components/WaterTracker.tsx
@@ -1,26 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { BarChart } from 'react-native-gifted-charts';
 import { useDarkMode } from '../DarkModeContext';
 
 const WaterTracker = () => {
-  const {isEnabled} = useDarkMode(); // Récupérer la valeur du mode sombre depuis le contexte
-
+  const { isEnabled } = useDarkMode();
   const [waterCount, setWaterCount] = useState(0);
+  const [barData, setBarData] = useState([]);
+
+  useEffect(() => {
+    const today = "Aujourd'hui"; // Libellé pour la date actuelle
+    const previousDays = generatePreviousDays();
+    const data = previousDays.concat([{ value: waterCount, label: today , frontColor: "lightgray"}]);
+    setBarData(data);
+  }, [waterCount]);
 
   const incrementWaterCount = () => {
     setWaterCount(prevCount => prevCount + 1);
   };
 
+  const generatePreviousDays = () => {
+    const previousDays = [
+      { value: 3, label: 'Lun' },
+      { value: 5, label: 'Mar' },
+      { value: 4, label: 'Mer' },
+      { value: 2, label: 'Jeu' }
+    ];
+    return previousDays;
+  };
+
   return (
     <View style={[styles.container, isEnabled ? styles.darkContainer : null]}>
       <Text style={[styles.title, isEnabled ? styles.darkTitle : null]}>Suivi de consommation d'eau</Text>
-      <Text style={[styles.text, isEnabled ? styles.darkText : null]}>Verres d'eau bus aujourd'hui : {waterCount}</Text>
+      <BarChart
+      barBorderRadius={4}
+        barWidth={50}
+        hideRules
+        noOfSections={5}
+        frontColor={ '#177AD5'}
+        data={barData}
+        yAxisThickness={0}
+        xAxisThickness={0}
+        showReferenceLine1
+        referenceLine1Position={10}
+        referenceLine1Config={{
+          color: 'red',
+          dashWidth: 2,
+          dashGap: 3,
+        }}
+      />
       <TouchableOpacity style={[styles.button, isEnabled ? styles.darkButton : null]} onPress={incrementWaterCount}>
         <Text style={styles.buttonText}>Ajouter un verre</Text>
       </TouchableOpacity>
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -40,18 +75,11 @@ const styles = StyleSheet.create({
   darkTitle: {
     color: 'white',
   },
-  text: {
-    fontSize: 18,
-    marginBottom: 20,
-    color: 'black',
-  },
-  darkText: {
-    color: 'white',
-  },
   button: {
     backgroundColor: 'blue',
     padding: 10,
     borderRadius: 5,
+    marginTop: 20,
   },
   darkButton: {
     backgroundColor: 'darkblue',
